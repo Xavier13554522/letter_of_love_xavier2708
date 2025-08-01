6b7cee429253769b205d95d643958430dabaf98b
Xavier Gomez <xavierggomey@gmail.com>
2025-08-01 13:04:00 -0400

    update_dependencias

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { Loading } from './components/loading/loading.jsx';
 import { Download } from './components/letter_of_love/download.jsx';
 import "./components/loading/animation.css"
 import { AboutMe } from './components/about me/about_me.jsx';
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   // State to hold the letter data
@@ -24,6 +25,7 @@ function App() {
   return (
     <>
     <Suspense fallback={<Loading/>}>
+    <Analytics/>
     <header>
       <img src="/Heart_corazón.svg" alt=""  className='img_title'/>
       <h1>Letter Of Love</h1>
